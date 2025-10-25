76f9734c336f436ba53b58d8927997de4ec48fe2
reygnn <reygnn@proton.me>
2025-10-25 08:12:28 +0200

    SwipeActions Anordnung korrigiert.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 26
-        versionName = "0.99.24 (a16+)"
+        versionCode = 28
+        versionName = "0.99.26 (a16+)"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
