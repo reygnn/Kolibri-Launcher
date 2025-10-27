6efb5c7ed099d5ec4ff214c8af72e9da596cb07e
reygnn <reygnn@proton.me>
2025-10-27 18:43:16 +0100

    versionCode = 29, versionName = "0.99.27"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 28
-        versionName = "0.99.26"
+        versionCode = 29
+        versionName = "0.99.27"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
