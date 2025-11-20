a0831c82fd14d64bbac51fed05e8fcd611d44212
user <reygnn@proton.me>
2025-11-20 18:27:02 +0100

    SplitScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,8 +80,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 41
-        versionName = "0.99.39 rc10"
+        versionCode = 42
+        versionName = "0.99.40 rc11"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
