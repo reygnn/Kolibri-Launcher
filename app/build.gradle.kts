262115d256f8e33dc0393bc4c120747d3f25ef6e
user <reygnn@proton.me>
2025-12-19 01:33:23 +0100

    Lumo Verbesserungen.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 63
-        versionName = "0.99.48.13 rc19.13"
+        versionCode = 64
+        versionName = "0.99.48.14 rc19.14"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
