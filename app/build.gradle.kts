2f155d200a63b5671a524487ad2d59cb0d256b22
user <reygnn@proton.me>
2026-01-31 10:33:38 +0100

    neuer GitHub Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 67
-        versionName = "0.99.49"
+        versionCode = 68
+        versionName = "0.99.50"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
