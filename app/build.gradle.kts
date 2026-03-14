80bb8b08f26d18eda89472d9c2f3f6236c0397f6
user <reygnn@proton.me>
2026-03-14 15:39:17 +0100

    build.gradle für v0.99.53 vorbereitet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 70
-        versionName = "0.99.52"
+        versionCode = 71
+        versionName = "0.99.53"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
