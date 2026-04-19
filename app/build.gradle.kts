d6a1577c318fcccc17849c682031bacc3f63017d
user <reygnn@proton.me>
2026-04-19 16:35:29 +0200

    WallpaperSupport stabilisiert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,8 +81,8 @@ android {
         applicationId = "com.github.reygnn.kolibri_launcher"
         minSdk = 36 // DO NOT CHANGE !!!
         targetSdk = 36 // DO NOT CHANGE !!!
-        versionCode = 76
-        versionName = "0.99.58"
+        versionCode = 77
+        versionName = "0.99.59"
 
         // BuildConfig-Felder erstellen
         buildConfigField(
